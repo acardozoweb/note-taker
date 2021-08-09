a9b288d4d59027bdd1c5215789135f2396a601a6
anthonycc <anthonyccardozo@gmail.com>
2021-08-08 21:40:21 -0400

    moved routes to route files

--- Routes/htmlRoutes/htmlRoutes.js
+++ Routes/htmlRoutes/htmlRoutes.js
@@ -3,4 +3,12 @@
 
 
 
+////////// HTML ROUTES //////////
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
--- server.js
+++ server.js
@@ -18,52 +18,6 @@ app.use(express.static('public'));
 
 
 
-////////// API ROUTES //////////
-
-app.get('/api/notes', (req, res) => {
-    // create variable from db.json
-    let notes = JSON.parse(fs.readFileSync('./data/db.json', 'utf8'));
-    // return to client
-    return res.json(notes);
-});
-
-app.post('/api/notes', (req, res) => {
-    let newNote = req.body;
-    // create variable from db.json
-    let notes = JSON.parse(fs.readFileSync('./data/db.json', 'utf8'));
-    // generate unique id with uuid
-    newNote.id = uuidv4();
-    // push newNote to notes variable
-    notes.push(newNote);
-    // put notes variable in db.json file
-    fs.writeFileSync('./data/db.json', JSON.stringify(notes));
-    // return to client
-    return res.json(notes);
-});
-
-app.delete('./api/notes/:id', (req, res) => {
-    // create variable to delete
-    let deleteNote = req.params.id;
-    // create variable from db.json
-    let notes = JSON.parse(fs.readFileSync('./data/db.json', 'utf8'));
-    // filter by id to make newNotes array
-    let newNotes = notes.filter(note => note.id !== deleteNote);
-    // put newNotes array into db.json
-    fs.writeFileSync('./data/db.json', JSON.stringify(newNotes));
-    // return to client
-    return res.json(newNotes);
-});
-
-
-////////// HTML ROUTES //////////
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
 
 ////////// LISTENER //////////
 
