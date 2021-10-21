ae84c92c5817d3beed2bfc47d8483509c6bd6b34
anthonycc <anthonyccardozo@gmail.com>
2021-10-21 15:20:27 -0400

    works locally again, now to redeploy again

--- Routes/apiRoutes/apiRoutes.js
+++ Routes/apiRoutes/apiRoutes.js
@@ -12,34 +12,23 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 ////////// API ROUTES //////////
 
-// router.get('/notes', (req, res) => {
-//     //creates variable from db.json file
-//     let notes = JSON.parse(fs.writeFileSync('./Data/db.json', 'utf8'));
-//     console.log('notes', notes);
-//     console.log('notesType', typeOf(notes));
-//     //return notes to client
-//     return res.json(notes);
-// });
-
-router.get('/notes', async (req, res) => {
+router.get('/notes', (req, res) => {
     //creates variable from db.json file
-    let notes = await readFileAsync('./Data/db.json', 'utf8');
-    await console.log('notes', notes);
-    await console.log('notesType', typeOf(notes));
+    let notes = JSON.parse(fs.readFileSync('./data/db.json', 'utf8'));
     //return notes to client
     return res.json(notes);
 });
 
-router.post('/note', (req, res) => {
+router.post('/notes', (req, res) => {
     let newNote = req.body;
     //generate unique id with uuid package
     newNote.id = uuidv4();
     //create variable from db.json file
-    let notes = (fs.writeFileSync('./Data/db.json', JSON.stringify()));
+    let notes = JSON.parse(fs.readFileSync('./data/db.json', 'utf8'));
     //push newNote to notes variable 
     notes.push(newNote);
     //write notes variable to db.json file
-    fs.writeFileSync('./Data/db.json', JSON.stringify(notes));
+    fs.writeFileSync('./data/db.json', JSON.stringify(notes));
     //return notes to client
     return res.json(notes);
 });
@@ -48,11 +37,11 @@ router.delete('/notes/:id', (req, res) => {
     //creates variable from file to delete
     let deleteNote = req.params.id;
     //creates variable from db.json file
-    let notes = JSON.parse(fs.readFileSync('./Data/db.json', 'utf8'));
+    let notes = JSON.parse(fs.readFileSync('./data/db.json', 'utf8'));
     //removes/filters deleteFile from notes
     let newNotes = notes.filter(file => file.id !== deleteNote);
     //write notes variable to db.json file
-    fs.writeFileSync('./Data/db.json', JSON.stringify(newNotes));
+    fs.writeFileSync('./data/db.json', JSON.stringify(newNotes));
     //return notes to client
     return res.json(newNotes);
 });
